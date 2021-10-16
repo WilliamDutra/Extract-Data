e9c0f7d44c6b417cf0c6923c5ccc88c2b3601066
William Dutra <will.dutra26@gmail.com>
2021-10-16 19:18:29 -0300

    criacao da infraestrutura do projeto

---
+++ ExtractData.Domain/Interfaces/ISQL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace ExtractData.Domain.Interfaces
+{
+    public interface ISQL
+    {
+        public SqlConnection CreateConnection(string strConnection);
+
+        public SqlCommand CreateCommand(string Command, CommandType CommandType);
+
+    }
+}
---
+++ ExtractData.Domain/SQL.cs
@@ -0,0 +1,44 @@
+﻿using ExtractData.Domain.Interfaces;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ExtractData.Domain
+{
+    public class SQL : ISQL
+    {
+
+        public SqlConnection CreateConnection(string strConnection)
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(strConnection);
+                connection.Open();
+                return connection;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public SqlCommand CreateCommand(string Command, CommandType CommandType)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandText = Command;
+                command.CommandType = CommandType;
+
+                return command;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
