e1eaa1eba9c0bdc42c2dd0b402ed7fa46c0b159c
William Dutra <will.dutra26@gmail.com>
2021-10-20 20:38:44 -0300

    adicionado tela para salvar o script gerado

--- ExtractData/ViewModels/MainWindowViewModel.cs
+++ ExtractData/ViewModels/MainWindowViewModel.cs
@@ -7,7 +7,9 @@ using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Text;
+using System.Windows;
 
 namespace ExtractData.UI.ViewModels
 {
@@ -18,35 +20,39 @@ namespace ExtractData.UI.ViewModels
 
         private ShowDatabase _DatabaseSelected;
 
-        public ShowDatabase DatabaseSelected 
+        public ShowDatabase DatabaseSelected
         {
-            get { return _DatabaseSelected; } set { SetProperty(ref _DatabaseSelected, value); Tables = LoadTables(); } 
+            get { return _DatabaseSelected; }
+            set { SetProperty(ref _DatabaseSelected, value); Tables = LoadTables(); }
         }
 
         private ShowTable _TableSelected;
 
-        public ShowTable TableSelected 
+        public ShowTable TableSelected
         {
-            get { return _TableSelected; } set { SetProperty(ref _TableSelected, value); }
+            get { return _TableSelected; }
+            set { SetProperty(ref _TableSelected, value); }
         }
 
         private ObservableCollection<ShowDatabase> _Databases;
 
-        public ObservableCollection<ShowDatabase> Databases 
+        public ObservableCollection<ShowDatabase> Databases
         {
-            get { return _Databases; } set { SetProperty(ref _Databases, value); }
+            get { return _Databases; }
+            set { SetProperty(ref _Databases, value); }
         }
 
         private ObservableCollection<ShowTable> _Tables;
 
-        public ObservableCollection<ShowTable> Tables 
+        public ObservableCollection<ShowTable> Tables
         {
-            get { return _Tables; } set { SetProperty(ref _Tables, value); } 
+            get { return _Tables; }
+            set { SetProperty(ref _Tables, value); }
         }
 
         private string _Provider;
 
-        public string Provider 
+        public string Provider
         {
             get { return _Provider; }
             set { SetProperty(ref _Provider, value); Databases = LoadDatabases(); }
@@ -54,35 +60,39 @@ namespace ExtractData.UI.ViewModels
 
         private string _ConnectionString;
 
-        public string ConnectionString 
+        public string ConnectionString
         {
-            get { return _ConnectionString; } set { SetProperty(ref _ConnectionString, value);  } 
+            get { return _ConnectionString; }
+            set { SetProperty(ref _ConnectionString, value); }
         }
 
         private bool _IsTexto;
 
-        public bool IsTexto 
+        public bool IsTexto
         {
-            get { return _IsTexto; } set { SetProperty(ref _IsTexto, value); } 
+            get { return _IsTexto; }
+            set { SetProperty(ref _IsTexto, value); }
         }
 
         private bool _IsExcel;
 
-        public bool IsExcel 
+        public bool IsExcel
         {
-            get { return _IsExcel; } set { SetProperty(ref _IsExcel, value); } 
+            get { return _IsExcel; }
+            set { SetProperty(ref _IsExcel, value); }
         }
 
         private IMySql _MySql;
 
         public MainWindowViewModel()
         {
-            ExtrairCommand = new Command(() => GenerateSQLTextScripts(DatabaseSelected.Database, TableSelected.Table));
+            
         }
 
         public MainWindowViewModel(IMySql MySql)
         {
             _MySql = MySql;
+            ExtrairCommand = new Command(() => GenerateSQLTextScripts(DatabaseSelected.Database, TableSelected.Table));
         }
 
         private ObservableCollection<ShowDatabase> LoadDatabases()
@@ -115,17 +125,17 @@ namespace ExtractData.UI.ViewModels
 
         private void GenerateSQLTextScripts(string Database, string Table)
         {
-            
+
             if (IsTexto)
             {
                 var data = _MySql.ShowColumn(Database, Table);
                 var ScriptSQL = _MySql.GenerateScriptsSql(data, Database, Table);
 
-                var saveFileDialog = new SaveFileDialog();
-                saveFileDialog.ShowDialog();
-                var caminho = saveFileDialog.FileName;
-                
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                if (saveFileDialog.ShowDialog() == true)
+                    File.WriteAllText(saveFileDialog.FileName, ScriptSQL);
 
+                MessageBox.Show("Arquivo salvo com sucesso!");
             }
         }
 
